85c582343c5b9f2c4d1b9d3ff8379a2da75bc893
codeturd@outlook.com <codeturd@outlook.com>
2019-06-01 23:34:38 -0700

    Update 0.2b  - Cleaned up code using CodeMaid. Fixed typo in hint. Increased readability in dialogue. Added lineBreaker to gameOver screen.

--- ConsoleTBA/Program.cs
+++ ConsoleTBA/Program.cs
@@ -1,38 +1,33 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Resources;
 
 namespace ConsoleTBA
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             string playerName = "";
 
             string playerGreeting = "Welcome, let's start off by signing your name using the keyboard tool: ";
 
             string gameTitle = @"  \  |_ _|  ___| |   |__ __| |   |  _ \  |     ____|
-   \ |  |  |     |   |   |   |   | |   | |     __|  
- |\  |  |  |   | ___ |   |   ___ | |   | |     |    
+   \ |  |  |     |   |   |   |   | |   | |     __|
+ |\  |  |  |   | ___ |   |   ___ | |   | |     |
 _| \_|___|\____|_|  _|  _|  _|  _|\___/ _____|_____|";
 
-            string gameContinued = @"  ___|  _ \   \  |__ __|_ _|  \  | |   | ____| __ \ 
+            string gameContinued = @"  ___|  _ \   \  |__ __|_ _|  \  | |   | ____| __ \
  |     |   |   \ |   |    |    \ | |   | __|   |   |
  |     |   | |\  |   |    |  |\  | |   | |     |   |
 \____|\___/ _| \_|  _|  ___|_| \_|\___/ _____|____/ ";
 
             string gameOver = @" ____| \   _ _| |     |   |  _ \  ____| | |    \  |  _ \__ __|
- |    _ \    |  |     |   | |   | __|   | |     \ | |   |  |  
- __| ___ \   |  |     |   | __ <  |    _|_|   |\  | |   |  |  
-_| _/    _\___|_____|\___/ _| \_\_____|_)_)  _| \_|\___/  _|  
-                                                              
-  ___|  |   |  ___|  ___| ____|  ___|  ___|  ____| |   | |    
-\___ \  |   | |     |     __|  \___ \\___ \  |     |   | |    
-      | |   | |     |     |          |     | __|   |   | |    
+ |    _ \    |  |     |   | |   | __|   | |     \ | |   |  |
+ __| ___ \   |  |     |   | __ <  |    _|_|   |\  | |   |  |
+_| _/    _\___|_____|\___/ _| \_\_____|_)_)  _| \_|\___/  _|
+
+  ___|  |   |  ___|  ___| ____|  ___|  ___|  ____| |   | |
+\___ \  |   | |     |     __|  \___ \\___ \  |     |   | |
+      | |   | |     |     |          |     | __|   |   | |
 _____/ \___/ \____|\____|_____|_____/_____/ _|    \___/ _____|";
 
             string journeyPart1 = @"     |  _ \  |   |  _ \   \  | ____|\ \   /    _ \__ __|  _ |
@@ -53,18 +48,15 @@ _____/ \___/ \____|\____|_____|_____/_____/ _|    \___/ _____|";
 
             string hint = "You have a feeling these jewels may hold unknown powers and possibly dictate your journey. Choose wisely.";
 
-            string enterPrompt = "Hit (Enter) to make your choice or receieve a hint.";
+            string enterPrompt = "Hit (Enter) to make your choice or receive a hint.";
 
             ////const string format = "{0,-10} {1,10}";
-
 
             int guessAmount = 0;
 
             int guessLimit = 5;
 
             bool outOfGuesses = false;
-
-
 
             ////Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + ("test".Length / 2)) + "}", "test"));
             Console.Write(gameTitle);
@@ -77,8 +69,9 @@ _____/ \___/ \____|\____|_____|_____/_____/ _|    \___/ _____|";
             Console.Write(gameContinued);
             Console.WriteLine();
             Console.Write(lineBreaker);
-            Console.WriteLine();
-            Console.Write("\nThank you " + playerName + "! You are about to embark on a wonderful adventure. One with many great twists, characters, and events. All leading to an epic grand finale beyond your wildest imagination! Before we venture forth, I have another less" +
+            Console.Write("\nThank you " + playerName + "! You are about to embark on a wonderful adventure. " +
+                "One with many great twists, characters, and events. All leading to an epic grand finale " +
+                "beyond your wildest imagination! Before we venture forth, I have another less" +
                 "\nintrusive question. I have 3 jewels, each vastly different from the other, still equally as beautiful in color.\n");
             Console.ForegroundColor = ConsoleColor.Blue;
             Console.Write(jewelBlue + ", ");
@@ -91,12 +84,10 @@ _____/ \___/ \____|\____|_____|_____/_____/ _|    \___/ _____|";
             Console.Write(jewelRed + ". ");
             Console.ResetColor();
             Console.Write(enterPrompt);
-               
 
             while (!guess.Contains("GREEN") && !guess.Contains("RED") && !guess.Contains("BLUE") && !outOfGuesses)
 
-                 {
-
+            {
                 if (guessAmount < guessLimit)
                 {
                     Console.WriteLine();
@@ -107,16 +98,11 @@ _____/ \___/ \____|\____|_____|_____/_____/ _|    \___/ _____|";
                     Console.ForegroundColor = ConsoleColor.DarkGray;
                     Console.Write(hint);
                     Console.ResetColor();
-                } 
-
-
-
-
+                }
                 else
                 {
                     outOfGuesses = true;
                     break;
-                  
                 }
             }
 
@@ -125,8 +111,8 @@ _____/ \___/ \____|\____|_____|_____/_____/ _|    \___/ _____|";
                 Console.Clear();
                 Console.Write(gameOver);
                 Console.WriteLine();
-                Console.Write("\nYou are lose! Just kidding, try again. " + guessAmount +  " failed attempts, are you trying to break a record?");
-
+                Console.Write(lineBreaker);
+                Console.Write("\nYou are lose! Just kidding, try again. " + guessAmount + " failed attempts, are you trying to break a record?");
             }
             else
             {
@@ -136,10 +122,7 @@ _____/ \___/ \____|\____|_____|_____/_____/ _|    \___/ _____|";
                 Console.Write(lineBreaker);
                 Console.WriteLine();
                 Console.WriteLine("Marverlous choice, you store the " + (guess.ToLower()) + " jewel in your coin pouch for safe keeping.");
-                
             }
-
-
 
             Console.ReadLine();
         }
