ed9758ca6f6a41b6fea47fe1a4185dc2bc90b538
codeturd@outlook.com <codeturd@outlook.com>
2019-06-02 17:13:34 -0700

    Update 0.2c - Various tweaks.

--- ConsoleTBA/Program.cs
+++ ConsoleTBA/Program.cs
@@ -1,19 +1,33 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using Figgle;
+
 
 namespace ConsoleTBA
 {
     internal class Program
+
     {
+    
         private static void Main(string[] args)
         {
+
+            Console.OutputEncoding = Encoding.UTF8;
+
             string playerName = "";
 
-            string playerGreeting = "Welcome, let's start off by signing your name using the keyboard tool: ";
+            
 
-            string gameTitle = @"  \  |_ _|  ___| |   |__ __| |   |  _ \  |     ____|
-   \ |  |  |     |   |   |   |   | |   | |     __|
- |\  |  |  |   | ___ |   |   ___ | |   | |     |
-_| \_|___|\____|_|  _|  _|  _|  _|\___/ _____|_____|";
+            string playerGreeting = "MyStErIoUs Voice: I see your slumber has come to an end stranger, woefully time is not our ally. If you want to know why you lay in a stone cell waiting death. " +
+                "Put your otherwise misguided human trust in me and sign your name to this contract so I may pass on a physical possesion.Currently in my ethereal state, I am unable to help you out of this predicament. " +
+                "My current form is non-permanent, your consciousness returns and I fear this near-death experience is the catalyst to our communcation.";
+
+            string gameTitle = "NIGHTHOLE";
+
+            string gameTitle2 = @"";
 
             string gameContinued = @"  ___|  _ \   \  |__ __|_ _|  \  | |   | ____| __ \
  |     |   |   \ |   |    |    \ | |   | __|   |   |
@@ -58,21 +72,32 @@ _____/ \___/ \____|\____|_____|_____/_____/ _|    \___/ _____|";
 
             bool outOfGuesses = false;
 
-            ////Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + ("test".Length / 2)) + "}", "test"));
-            Console.Write(gameTitle);
+            ////Console.WriteLine(string.Format("{0," + ((Console.WindowWidth / 2) + (gameTitle.Length / 2)) + "}", gameTitle));
+
+            ////////Console.BackgroundColor = ConsoleColor.Yellow;
+
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(
+                FiggleFonts.SBlood.Render(gameTitle));
+            Console.ResetColor();
             Console.WriteLine();
             Console.Write(lineBreaker);
             Console.WriteLine();
             Console.Write(playerGreeting);
+            Console.WriteLine();
+            Console.Write(string.Format("{0," + ((Console.WindowWidth / 2) + ("Sign your name on the contract and hit (Enter) if you agree: ".Length / 2)) + "}", "Sign your name on the contract and hit (Enter) if you agree: "));
             playerName = Console.ReadLine();
+            ////(You groggily lift your arm to your left eye to learn is now closed and scarred over. You hear several footsteps coming towards you. With blood on your thumbs neighboring fingers from your unanswered eye
+            ////Wound. You sign your name on the cold floor to the sound of your heart racing.)
             Console.Clear();
             Console.Write(gameContinued);
             Console.WriteLine();
             Console.Write(lineBreaker);
             Console.Write("\nThank you " + playerName + "! You are about to embark on a wonderful adventure. " +
-                "One with many great twists, characters, and events. All leading to an epic grand finale " +
-                "beyond your wildest imagination! Before we venture forth, I have another less" +
-                "\nintrusive question. I have 3 jewels, each vastly different from the other, still equally as beautiful in color.\n");
+            "One with many great twists, characters, and events. All leading to an epic grand finale " +
+            "beyond your wildest imagination! Before we venture forth, I have another less" +
+            "\nintrusive question. I have 3 jewels, each vastly different from the other, still equally as beautiful in color.\n");
             Console.ForegroundColor = ConsoleColor.Blue;
             Console.Write(jewelBlue + ", ");
             Console.ResetColor();
@@ -121,7 +146,7 @@ _____/ \___/ \____|\____|_____|_____/_____/ _|    \___/ _____|";
                 Console.WriteLine();
                 Console.Write(lineBreaker);
                 Console.WriteLine();
-                Console.WriteLine("Marverlous choice, you store the " + (guess.ToLower()) + " jewel in your coin pouch for safe keeping.");
+                Console.WriteLine("Mysterious Man: Marverlous choice. (you store the " + (guess.ToLower()) + " jewel in your coin pouch for safe keeping.)");
             }
 
             Console.ReadLine();
