0e12e331e7d8fd8f8010a5b6e3871419338ba2db
codeturd@outlook.com <codeturd@outlook.com>
2019-06-03 02:09:03 -0700

    Update 0.4 - Code cleanup routine. Overhauled titleScreen. Added animated text along with double tap Space to start. Fixed alignment in MethodGameTitle. Renamed Mysterious man to Arcane Voice, along with rewritting dialogue.  Setup first game screen and fixed formatting along with added hints. More+

--- ConsoleTBA/Program.cs
+++ ConsoleTBA/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace ConsoleTBA
 {
@@ -12,9 +13,7 @@ namespace ConsoleTBA
 
             string playerName = "";
 
-            string playerGreeting = "Mysterious Voice: I see your slumber has come to an end stranger, woefully time is not our ally. If you want to know why you lay in a stone cell waiting death. " +
-                "Put your otherwise misguided human trust in me and sign your name to this contract so I may pass on a physical possesion.Currently in my ethereal state, I am unable to help you out of this predicament. " +
-                "My current form is non-permanent, your consciousness returns and I fear this near-death experience is the catalyst to our communcation. ";
+            string playerGreeting = "I see your sleep has come to an end stranger, woefully time is not our ally. Introductions are not important, so I request you listen closely. If you want to know why you lay in a stone cell waiting death in this decrepit castle. Humble me with your otherwise misguided human trust and sign your name to contract so I may pass on a physical possesion. In this ethereal state I am unable to aid in this plight and your consciousness returns. I fear this close to death experience is one-time catalyst to our communcation.  ";
 
             string gameTitle = @"███▄    █  ██▓  ▄████  ██░ ██ ▄▄▄█████▓ ██░ ██  ▒█████   ██▓    ▓█████
  ██ ▀█   █ ▓██▒ ██▒ ▀█▒▓██░ ██▒▓  ██▒ ▓▒▓██░ ██▒▒██▒  ██▒▓██▒    ▓█   ▀
@@ -25,6 +24,12 @@ namespace ConsoleTBA
 ░ ░░   ░ ▒░ ▒ ░  ░   ░  ▒ ░▒░ ░    ░     ▒ ░▒░ ░  ░ ▒ ▒░ ░ ░ ▒  ░ ░ ░  ░
    ░   ░ ░  ▒ ░░ ░   ░  ░  ░░ ░  ░       ░  ░░ ░░ ░ ░ ▒    ░ ░      ░
          ░  ░        ░  ░  ░  ░          ░  ░  ░    ░ ░      ░  ░   ░  ░";
+
+            string mysteriousVoice = @" __  ____   ______ _____ _____ ____  ___ ___  _   _ ____   __     _____ ___ ____ _____
+ |  \/  \ \ / / ___|_   _| ____|  _ \|_ _/ _ \| | | / ___|  \ \   / / _ \_ _/ ___| ____|  _
+ | |\/| |\ V /\___ \ | | |  _| | |_) || | | | | | | \___ \   \ \ / / | | | | |   |  _|   (_)
+ | |  | | | |  ___) || | | |___|  _ < | | |_| | |_| |___) |   \ V /| |_| | | |___| |___   _
+ |_|  |_| |_| |____/ |_| |_____|_| \_\___\___/ \___/|____/     \_/  \___/___\____|_____| (_)";
 
             string gameContinued = @"  ___|  _ \   \  |__ __|_ _|  \  | |   | ____| __ \
  |     |   |   \ |   |    |    \ | |   | __|   |   |
@@ -71,24 +76,36 @@ _____/ \___/ \____|\____|_____|_____/_____/ _|    \___/ _____|";
 
             ////Console.WriteLine(string.Format("{0," + ((Console.WindowWidth / 2) + (gameTitle.Length / 2)) + "}", gameTitle));
 
-            ////Console.BackgroundColor = ConsoleColor.Yellow;
-
             MethodGameTitle(gameTitle);
+            System.Threading.Thread.Sleep(15);
+            Console.Clear();
+            Console.ResetColor();
+            ////////Console.WriteLine(mysteriousVoice);
+            ///
 
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.WriteLine("ARCANE VOICE: ");
             Console.ResetColor();
-            Console.Write(lineBreaker);
-            Console.WriteLine();
-            Console.Write(playerGreeting);
+
+            //////Console.Write(playerGreeting);
+
+            Console.Write(Wrap1(playerGreeting, 118));
             Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine();
-            Console.Write("Sign your name on the contract and hit (Enter) if you agree: ");
+            Console.Write("Sign your name to contract and hit ( enter ) if you accept: ");
             Console.ResetColor();
+
             playerName = Console.ReadLine();
-            ////(You groggily lift your arm to your left eye to learn is now closed and scarred over. You hear several footsteps coming towards you. With blood on your thumbs neighboring fingers from your unanswered eye
-            ////Wound. You sign your name on the cold floor to the sound of your heart racing.) As you finish the final swoop on your signature, you see the barely ghost-like entity dissappear. Below where his voice was lay 3 jewels.
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write(Wrap1("(You groggily lift your arm to your left eye to learn it's now closed and scarred over in blood. You hear several footsteps coming towards your cell. " +
+                "With blood on your thumbs neighboring fingers, you sign your name on the cold floor to the sound of your heart racing in fear.) " +
+                "As you finish the final character in your signature, the wind chills and the ghost-like entity dissappears. You then faintly hear words in a forgien language to man. " +
+                "Below where the voice lingers lay 3 radiant jewels.)", 118));
+            Console.ResetColor();
             Console.WriteLine();
-            Console.Write(gameContinued);
+            /////////Console.Write(gameContinued);
             Console.WriteLine();
+            Console.ReadKey(true);
             Console.Write(lineBreaker);
             /////Console.Write("\nThank you " + playerName + "! You are about to embark on a wonderful adventure. " +
             ////"One with many great twists, characters, and events. All leading to an epic grand finale " +
@@ -157,14 +174,32 @@ _____/ \___/ \____|\____|_____|_____/_____/ _|    \___/ _____|";
             Console.ForegroundColor = ConsoleColor.DarkYellow;
             Console.WriteLine();
             Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
             Console.WriteLine(gameTitle);
             Console.ResetColor();
             Console.WriteLine();
             Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
 
-            for (int i = 0; i < "Double tap [spacebar] to start ".PadLeft(50).Length; i++)
+            Console.Write("(Text Based Adventure Game.)".PadLeft(48));
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            for (int i = 0; i < "Double tap [ spacebar ] to start ".PadLeft(51).Length; i++)
             {
-                Console.Write("Double tap [spacebar] to start ".PadLeft(50)[i]);
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.Write("Double tap [ spacebar ] to start ".PadLeft(51)[i]);
+                Console.ResetColor();
                 System.Threading.Thread.Sleep(70);
             }
 
@@ -174,7 +209,21 @@ _____/ \___/ \____|\____|_____|_____/_____/ _|    \___/ _____|";
 
             Console.ResetColor();
             Console.ReadKey();
-            Console.Clear();
+            ////Console.Clear();
+            ///
+        }
+
+        private static string Wrap1(string text, int max)
+        {
+            string[] inputLines = text.Replace("\r", string.Empty).Split('\n');
+            StringBuilder output = new StringBuilder();
+
+            foreach (string x in inputLines)
+            {
+                output.Append(Regex.Replace(x, @"(^| +)([^\n]{0," + max + @"}(?![\w\p{P}]))", "$2" + Environment.NewLine, RegexOptions.Multiline));
+            }
+
+            return output.ToString();
         }
     }
 }
